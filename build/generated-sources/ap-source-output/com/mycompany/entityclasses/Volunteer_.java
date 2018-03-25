960777baf50c8d1ddc7c7f81ecad14d553fd4662
Andres Pico <andresjpico@gmail.com>
2018-03-25 17:02:16 -0400

    Managed to remove extra spacing between body and footer in dashboard. Will try to place datatables in a panel grid now but make no guarantees of it working.

--- build/generated-sources/ap-source-output/com/mycompany/entityclasses/Volunteer_.java
+++ build/generated-sources/ap-source-output/com/mycompany/entityclasses/Volunteer_.java
@@ -3,7 +3,7 @@ package com.mycompany.entityclasses;
 import javax.annotation.Generated;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2018-03-25T14:01:19")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2018-03-25T16:58:53")
 @StaticMetamodel(Volunteer.class)
 public class Volunteer_ extends Users_ {
 
