ae7aee5feb6aed18f6a64868b9770b328c7adb18
Andres Pico <andresjpico@gmail.com>
2018-03-24 23:55:28 -0400

    - Added a few more tweaks on the user interface to improve it - Added a primefaces theme to enhance the UI even further

--- build/generated-sources/ap-source-output/com/mycompany/entityclasses/Organization_.java
+++ build/generated-sources/ap-source-output/com/mycompany/entityclasses/Organization_.java
@@ -3,7 +3,7 @@ package com.mycompany.entityclasses;
 import javax.annotation.Generated;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2018-03-24T13:39:53")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2018-03-24T23:42:11")
 @StaticMetamodel(Organization.class)
 public class Organization_ extends Users_ {
 
--- build/generated-sources/ap-source-output/com/mycompany/entityclasses/Volunteer_.java
+++ build/generated-sources/ap-source-output/com/mycompany/entityclasses/Volunteer_.java
@@ -3,7 +3,7 @@ package com.mycompany.entityclasses;
 import javax.annotation.Generated;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2018-03-24T13:39:53")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2018-03-24T23:42:11")
 @StaticMetamodel(Volunteer.class)
 public class Volunteer_ extends Users_ {
 
